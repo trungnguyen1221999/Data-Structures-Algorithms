a2e6811a33c30c6325a9b0ced4cde42b19e4fa9f
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-12-02 09:55:02 +0200

    Update index.js

--- Custom Array/index.js
+++ Custom Array/index.js
@@ -10,7 +10,37 @@ class MyArray {
         return this.length;
 
     }
+    get(index) {
+        return this.data[index];
+
+    }
+    pop()
+    {   
+        delete this.data[this.length - 1];
+        this.length--
+        return this.length;
+    }
+    shift() {
+        
+        if (this.length === 0) return undefined;
+        const firstItem = this.data[0];
+        for (let i = 0; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length-1]
+        this.length--;
+        return firstItem;
 }
+delete(index) {
+     if (index < 0 || index >= this.length) return undefined;
+    const item = this.data[index];
+     for (let i = index; i < this.length - 1; i++) {
+         this.data[i] = this.data[i + 1];
+     }
+     delete this.data[this.length - 1];
+     this.length--;
+     return item;
+}}
 
 
 const myNewArray = new MyArray();
@@ -19,4 +49,13 @@ myNewArray.push('hi')
 myNewArray.push('hoi')
 myNewArray.push('lam')
 myNewArray.push('gi')
+  myNewArray.push('ha')
+
+console.log(myNewArray.get(2))
 console.log(myNewArray);
+
+// myNewArray.pop()
+// myNewArray.shift()
+console.log(myNewArray.delete(1));
+console.log(myNewArray);
+
